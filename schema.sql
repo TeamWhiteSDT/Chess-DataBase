effbd218628ac5ad056353593cbc2add60c20ffe
Mikhail Malyarenko <malyarenko.md@gmail.com>
2022-12-11 22:22:50 +0300

    dev: add quotes, del reserved field names, add cascade delete

--- schema.sql
+++ schema.sql
@@ -5,62 +5,60 @@
  * Schema of online chess data base
  */
 
-USE chess_db;
+USE chess_db
 
 -- Player entiry
 
 CREATE TABLE IF NOT EXISTS player_table (
-    id              INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
-    name            VARCHAR(20) NOT NULL DEFAULT 'Player',
-    picture         BLOB
+    `id`            INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+    `name`          VARCHAR(20) NOT NULL DEFAULT 'Player',
+    `picture`       BLOB
 );
 
 -- Friend List entity
 
 CREATE TABLE IF NOT EXISTS friend_list_table (
-    id              INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
-    player_1_id     INT UNSIGNED,
-    player_2_id     INT UNSIGNED,
+    `id`            INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+    `player_1_id`   INT UNSIGNED,
+    `player_2_id`   INT UNSIGNED,
 
-    FOREIGN KEY (player_1_id) REFERENCES player_table (id),
-    FOREIGN KEY (player_2_id) REFERENCES player_table (id),
+    FOREIGN KEY (`player_1_id`)
+        REFERENCES `player_table` (`id`)
+        ON DELETE CASCADE,
+    FOREIGN KEY (`player_2_id`)
+        REFERENCES `player_table` (`id`)
+        ON DELETE CASCADE
 );
 
 -- Game entity
 
 /*
- * Game status enumeration:
+ * Game state enumeration:
  *      Planned     0
  *      Active      1
  *      Suspended   2
- *      Black Win   3
- *      White Win   4
+ *      White Win   3
+ *      Black Win   4
  *      Draw        5
  */
 
-CREATE TABLE IF NOT EXISTS game_table (
-    id              INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
-    w_player_id     INT UNSIGNED,
-    b_player_id     INT UNSIGNED,
-    status          TINYINT UNSIGNED DEFAULT 0,
+CREATE TABLE IF NOT EXISTS `game_table` (
+    `id`            INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+    `w_player_id`   INT UNSIGNED,
+    `b_player_id`   INT UNSIGNED,
+    `game_state`    TINYINT UNSIGNED DEFAULT 0,
 
-    FOREIGN KEY (w_player_id) REFERENCES player_table (id),
-    FOREIGN KEY (b_player_id) REFERENCES player_table (id),
+    FOREIGN KEY (`w_player_id`)
+        REFERENCES `player_table` (`id`)
+        ON DELETE CASCADE,
+    FOREIGN KEY (`b_player_id`)
+        REFERENCES `player_table` (`id`)
+        ON DELETE CASCADE,
 
-    CONSTRAINT status_range CHECK(status <= 5)
+    CONSTRAINT `game_state_range` CHECK(`game_state` <= 5)
 );
 
 -- Chess piece move entity
-
-/*
- * Chess piece type enumeration:
- *      Pawn        0
- *      Bishop      1
- *      Knight      2
- *      Rook        3
- *      Queen       4
- *      King        5
- */
 
  /*
   * Position notation:
@@ -71,19 +69,22 @@ CREATE TABLE IF NOT EXISTS game_table (
   *     h1 = 56, h2 = 57, ...,  h8 = 64
   */
 
-CREATE TABLE IF NOT EXISTS move_table (
-    id              BIGINT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
-    game_id         INT UNSIGNED,
-    player_id       INT UNSIGNED,
-    piece           TINYINT UNSIGNED,
-    from_position   SMALLINT UNSIGNED NOT NULL,
-    to_position     SMALLINT UNSIGNED NOT NULL,
-    number          SMALLINT UNSIGNED NOT NULL, /* Number of move in the game */
-    time_from_start INT UNSIGNED DEFAULT 0, /* Time from game started in seconds */
+CREATE TABLE IF NOT EXISTS `move_table` (
+    `id`                BIGINT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+    `game_id`           INT UNSIGNED,
+    `player_id`         INT UNSIGNED,
+    `from_position`     SMALLINT UNSIGNED NOT NULL,
+    `to_position`       SMALLINT UNSIGNED NOT NULL,
+    `num`               SMALLINT UNSIGNED NOT NULL, /* Number of move in the game */
+    `time_from_start`   INT UNSIGNED DEFAULT 0, /* Time from game started in seconds */
 
-    FOREIGN KEY (game_id) REFERENCES game_table (id),
-    FOREIGN KEY (player_id) REFERENCES player_table (id),
+    FOREIGN KEY (`game_id`)
+        REFERENCES `game_table` (`id`)
+        ON DELETE CASCADE,
+    FOREIGN KEY (`player_id`)
+        REFERENCES `player_table` (`id`)
+        ON DELETE CASCADE,
 
-    CONSTRAINT position_range CHECK(from_position < 64 AND to_position < 64),
-    CONSTRAINT piece CHECK(to_position <= 5)
+    CONSTRAINT `position_range` CHECK(`from_position` < 64 AND `to_position` < 64),
+    CONSTRAINT `piece` CHECK(`to_position` <= 5)
 );
